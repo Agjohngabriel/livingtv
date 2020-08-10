f3014894abf03ae55f5e9f1781f729f960cc156f
agjohngabriel <agjohngabriel@gmail.com>
2020-08-10 20:28:45 +0100

    initial commit

---
+++ about-us.php
@@ -0,0 +1,48 @@
+<?php 
+    include 'inc/header.php';
+    include 'inc/navbar.php';    
+?>
+    <!-- ##### Breadcumb Area Start ##### -->
+    <div class="breadcumb-area bg-img" style="background-image: url(assets/img/bg-img/breadcumb.jpg);">
+        <div class="bradcumbContent">
+            <h2>About Us</h2>
+        </div>
+    </div>
+    <!-- ##### Breadcumb Area End ##### -->
+
+    <!-- ##### About Us Area Start ##### -->
+    <section class="about-us-area mt-50 section-padding-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading text-center mx-auto wow fadeInUp" data-wow-delay="300ms">
+                        <span>The Best</span>
+                        <h3>We are the Living Film Tv Academy</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6 wow fadeInUp" data-wow-delay="400ms">
+                    <p>Cras vitae turpis lacinia, lacinia lacus non, fermentum nisi. Donec et sollicitudin est, in euismod. Morbi vel arcu gravida, iaculis lacus vel, posuere ipsum. Sed faucibus mauris vitae urna consectetur, sit amet maximus nisl sagittis. Ut in iaculis enim, et pulvinar mauris. Etiam tristique magna eget velit consectetur, a tincidunt velit dictum. Cras vulputate metus id felis ornare hendrerit. Maecenas sodales suscipit ipsum.</p>
+                </div>
+                <div class="col-12 col-md-6 wow fadeInUp" data-wow-delay="500ms">
+                    <p>Vitae turpis lacinia, lacinia lacus non, fermentum nisi. Donec et sollicitudin est, in euismod. Morbi vel arcu gravida, iaculis lacus vel, posuere ipsum. Sed faucibus mauris vitae urna consectetur, sit amet maximus nisl sagittis. Ut in iaculis enim, et pulvinar mauris. Etiam tristique magna eget velit consectetur, a tincidunt velit dictum. Cras vulputate metus id felis ornare hendrerit. Maecenas sodales suscipit ipsum.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="about-slides owl-carousel mt-100 wow fadeInUp" data-wow-delay="600ms">
+                        <img src="assets/img/bg-img/bg-3.jpg" alt="">
+                        <img src="assets/img/bg-img/bg-22.jpg" alt="">
+                        <img src="assets/img/bg-img/bg-11.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### About Us Area End ##### -->
+
+
+<?php 
+    include 'inc/footer.php';    
+?>
---
+++ inc/navbar.php
@@ -0,0 +1,76 @@
+<!-- Navbar Area -->
+        <div class="academy-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="academyNav">
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- close btn -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li><a href="index.php">Home</a></li>
+                                    <li><a href="/admission">Admission</a></li>
+                                    <li><a href="#">Programs</a>
+                                        <div class="megamenu">
+                                            <ul class="single-mega cn-col-4">
+                                                <li><h5>Areas of Studies</h5></li>
+                                                <li><a href="#">Film making</a></li>
+                                                <li><a href="#">Producing</a></li>
+                                                <li><a href="#">Cinematography</a></li>
+                                                <li><a href="#">Digital Editing</a></li>
+                                                <li><a href="#">Graphics Design</a></li>
+                                                <li><a href="#">Musical Theatre</a></li>
+                                                <li><a href="#">3D Animation & Visual Effects</a></li>
+                                            </ul>
+                                            <ul class="single-mega cn-col-4">
+                                                <li><h5>Summer Camps</h5></li>
+                                                <li><a href="#">Camps for Teens</a></li>
+                                                <li><a href="#">Camps for Kids</a></li>
+                                                <li><a href="#">Youths Online Workshop</a></li>
+                                                <li><a href="#">Online Workshops</a></li>
+                                            </ul>
+                                            <ul class="single-mega cn-col-4">
+                                                <li><h5>Degree Programs</h5></li>
+                                                <li><a href="#">Bachelor of Fine Arts</a></li>
+                                                <li><a href="#">Bachelor of Arts</a></li>
+                                                <li><a href="#">Associate of Fine Arts</a></li>
+                                                <li><a href="#">Master of Arts</a></li>
+                                                <li><a href="#">Master of Fine Arts</a></li>
+                                            </ul>
+                                            <div class="single-mega cn-col-4">
+                                                <img src="img/bg-img/bg-1.jpg" alt="">
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li><a href="about-us.php">About Us</a></li>
+                                    <li><a href="course.php">Course</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
+                                </ul>
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+
+                        <!-- Calling Info -->
+                        <div class="calling-info">
+                            <div class="call-center">
+                                <a href="tel:+2348033761664"><i class="icon-telephone-2"></i> <span>(+234) 0803 376 1664</span></a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
